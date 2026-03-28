2dfa01e5cae9382e850562f5583afedbecb78622
CJ <cjf8140@gmail.com>
2026-03-29 02:22:59 +0900

    comit

--- Part2/P2_CH10_multi_millis/P2_CH10_multi_millis.ino
+++ Part2/P2_CH10_multi_millis/P2_CH10_multi_millis.ino
@@ -1,5 +1,5 @@
 #define LED1 2
-#define LED2 4
+#define LED2 3
 
 #define DURATION_BLINK 500
 #define DURATION_FADING 1000
@@ -13,5 +13,5 @@ void setup()
 void loop()
 {
   digitalWrite(LED1, (millis() / (DURATION_BLINK / 2)) % 2);                  //DURATION_BLINK/2 만큼 켜지고 DURATION_BLINK/2 만큼 꺼지고
-  analogWrite(LED2, (sin(millis() / (2 * PI) * DURATION_FADING) + 1) * 127);  //켜졌다 꺼지기까지 DURATION FADING의 주기.
+  analogWrite(LED2, (int)((sin(2.0 * PI * millis() / DURATION_FADING) + 1.0) * 127.5));
 }
