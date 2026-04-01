65c475cfd0160992574410c40a428c3cced355a4
CJ <cjf8140@gmail.com>
2026-04-01 16:28:54 +0900

    가변저항서보 주석추가

--- Part2/2_9_servo_with_potentiometer/2_9_servo_with_potentiometer.ino
+++ Part2/2_9_servo_with_potentiometer/2_9_servo_with_potentiometer.ino
@@ -12,4 +12,9 @@ void setup()
 void loop()
 {
   my_servo.write(map(analogRead(A1), 0, 1023, 45, 135));
+
+
+  // v 이런식으로 변수로 분리 가능 v
+  // int angle_servo = map(analogRead(A1), 0, 1023, 45, 135);
+  // my_servo.write(angle_servo);
 }
