570992a4d9b6d45910fea3dc0c243eb774996e5d
SY Park <kyojis@empal.com>
2026-04-03 19:01:22 +0900

    Update 2_9_servo_with_potentiometer.ino

--- Part2/2_9_servo_with_potentiometer/2_9_servo_with_potentiometer.ino
+++ Part2/2_9_servo_with_potentiometer/2_9_servo_with_potentiometer.ino
@@ -4,17 +4,15 @@ Servo my_servo;
 void setup()
 {
   my_servo.attach(9);  //핀 연결
-  my_servo.write(90);
-
-  pinMode(A1, INPUT);
+  my_servo.write(90); // //가변저항핀모드 인풋 삭제
 }
 
 void loop()
 {
-  my_servo.write(map(analogRead(A1), 0, 1023, 45, 135));
+  int angle_servo = map(analogRead(A1), 0, 1023, 0, 180);
+  my_servo.write(angle_servo);
 
-
-  // v 이런식으로 변수로 분리 가능 v
-  // int angle_servo = map(analogRead(A1), 0, 1023, 45, 135);
-  // my_servo.write(angle_servo);
+  // v 분리된것을 기본으로하고 한 줄로도 할 수 있다는 코드 설명에서 v
+  //my_servo.write(map(analogRead(A1), 0, 1023, 0, 180)); 
+  // 
 }
