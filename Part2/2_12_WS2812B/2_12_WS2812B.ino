fb7b14a0fcec0079ed65d33f9763fdeeeb67d26a
SY Park <kyojis@empal.com>
2026-05-21 20:59:48 +0900

    여백정리

--- Part2/2_12_WS2812B/2_12_WS2812B.ino
+++ Part2/2_12_WS2812B/2_12_WS2812B.ino
@@ -3,21 +3,17 @@
 #define LED_PIN 3      // LED 스트립 연결 핀
 #define NUM_LEDS 1     // LED 개수
 #define BRIGHTNESS 50  
-
 CRGB leds[NUM_LEDS];
-
 int state = 0;
 
 void setup() {
   pinMode(2, INPUT_PULLUP);
-
-  FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
+  FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
   FastLED.setBrightness(BRIGHTNESS);
 
   leds[0] = CRGB(255,0,0);
   FastLED.show();
 }
-
 
 void loop() {
   if(digitalRead(2) == LOW) {
@@ -25,7 +21,6 @@ void loop() {
     if(state == 3) {
       state = 0;
     }
-    
     if(state == 0) {
       leds[0] = CRGB(255,0,0);
     }
--- Part2/2_9_servo_with_potentiometer/2_9_servo_with_potentiometer.ino
+++ Part2/2_9_servo_with_potentiometer/2_9_servo_with_potentiometer.ino
@@ -11,14 +11,13 @@ void setup()
 void loop()
 {
   int value_pot = analogRead(A1);
-  int angle_servo = map(value_pot, 0, 1023, 0, 180);
+  int angle_servo = map(value_pot, 0, 1023, 5, 175);
   my_servo.write(angle_servo);
 
   Serial.print("Pot: ");
   Serial.print(value_pot);
   Serial.print(", Angle: ");
   Serial.println(angle_servo);
-
   // v 분리된것을 기본으로하고 한 줄로도 할 수 있다는 코드 설명에서 v
   //my_servo.write(map(analogRead(A1), 0, 1023, 5, 175)); 
   // 
