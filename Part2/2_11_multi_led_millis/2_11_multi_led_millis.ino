b64d2d8118da2b876474efa16011f15db5f2d6cc
SY Park <kyojis@empal.com>
2026-05-23 19:07:28 +0900

    삼각파 code 추가

--- Part2/2_11_multi_led_millis/2_11_multi_led_millis.ino
+++ Part2/2_11_multi_led_millis/2_11_multi_led_millis.ino
@@ -1,8 +1,9 @@
 #define LED1 3
 #define LED2 5
 
-#define DURATION_BLINK 500
-#define PERIOD_FADING 1000
+#define DURATION_BLINK 500 // ①500 = 0.5sec duration
+#define PERIOD_TRI 4 // ②4 = 2.05 sec period (512ms * N)
+#define PERIOD_SIN 1000 // ③1000 = 1sec period
 
 void setup()
 {
@@ -14,7 +15,11 @@ void loop()
 {
   int value1 = (millis() / DURATION_BLINK) % 2;
   digitalWrite(LED1, value1);
-
-  int value2 = (int)((sin(TWO_PI * millis() / (float)PERIOD_FADING) + 1.0) * 127.5);
+  
+  int value2 = (millis() / PERIOD_TRI) % 511;
+  if (value2 > 255){
+    value2 = 510 - value2;
+  }
+//  int value2 = (int)((sin(TWO_PI * millis() / (float)PERIOD_SIN) + 1.0) * 127.5);
   analogWrite(LED2, value2);
 }
