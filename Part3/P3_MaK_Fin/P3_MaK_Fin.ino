3e6cba52328e9687cdb560605f4f145583053e5a
CJ <cjf8140@gmail.com>
2026-06-03 18:19:59 +0900

    Update P3_MaK_Fin.ino

--- Part3/P3_MaK_Fin/P3_MaK_Fin.ino
+++ Part3/P3_MaK_Fin/P3_MaK_Fin.ino
@@ -2,12 +2,12 @@
 
 #define PIN_TOUCH 2 // Red LED Yellow -- wire
 #define PIN_LED_COCKPIT 3 // War-W LED Orange wire
-#define PIN_LED_BACK 5 // Red LED Yellow -- wire
+#define PIN_LED_TOP 5 // Red LED Yellow -- wire
 #define PIN_LED_LEFT 6 // Yellow LED Yellow wire
 #define PIN_LED_FRONT 11 // Blue LED Blue wire
 
 #define PERIOD_SIN 3000 // Ori 1000 code from Police Zaku
-#define BRIGHT_SENSOR 40 // Red, Yellow LED
+#define BRIGHT_LED 80 // Red, Yellow LED
 
 Servo servo_hatch;      //Close 90,  Open 53 <- Ori 91,44
 int state=1;   // for switch toggle
@@ -18,25 +18,25 @@ void setup() {
   servo_hatch.attach(9);
   servo_hatch.write(90);
 
-  analogWrite(PIN_LED_LEFT, BRIGHT_SENSOR);
-  analogWrite(PIN_LED_BACK, BRIGHT_SENSOR);
+  analogWrite(PIN_LED_LEFT, BRIGHT_LED);
+  analogWrite(PIN_LED_TOP, BRIGHT_LED);
 }
 
 void loop() {
-  int led_value = (int) ((sin(TWO_PI * millis() / (float)PERIOD_SIN) + 1.0) * (140/2) );
+  int led_value = (int) ((sin(TWO_PI * millis() / (float)PERIOD_SIN) + 1.0) * BRIGHT_LED );
   analogWrite(PIN_LED_FRONT, led_value);
 
   if (digitalRead(PIN_TOUCH) == HIGH && state==1) {
-    for(int i = BRIGHT_SENSOR; i >= 0; i--) {
+    for(int i = BRIGHT_LED; i >= 0; i--) {
       analogWrite(PIN_LED_LEFT, i);
-      analogWrite(PIN_LED_BACK, i);
+      analogWrite(PIN_LED_TOP, i);
       delay(10);
     }
+
     delay(500);
+    analogWrite(PIN_LED_COCKPIT, BRIGHT_LED);
 
-    analogWrite(PIN_LED_COCKPIT, 80);
-
-      for(int i = 90; i >= 53; i--) {
+    for(int i = 90; i >= 53; i--) {
       servo_hatch.write(i);
       delay(40);
     }
@@ -48,16 +48,16 @@ void loop() {
       servo_hatch.write(i);
       delay(40);
       }
-    delay(530);
 
+    delay(500);
     analogWrite(PIN_LED_COCKPIT, 0);
 
-    for(int i = 0; i <= BRIGHT_SENSOR; i++) {
+    for(int i = 0; i <= BRIGHT_LED; i++) {
       analogWrite(PIN_LED_LEFT, i);
-      analogWrite(PIN_LED_BACK, i);
+      analogWrite(PIN_LED_TOP, i);
       delay(40);
     }
     state=1;
   }
 }
-//Elehobby3 Coding P3 Ma.K_V1.5 2026.05.04 By C.J
+//Elehobby3 Coding P3 Ma.K_V1.8 2026.03.04 By C.J, Kyoji
